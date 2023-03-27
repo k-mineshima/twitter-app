ba16b26027918a5fe7149bc2d49a8c21d4238613
峯島和希 <mineshimakazuki@mineshimakazukinoMacBook-Pro.local>
2023-03-28 00:55:08 +0900

    make name required

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
          :recoverable,
          :rememberable,
          :validatable
+
+  validates :name, presence: true
 end
--- db/migrate/20230327150905_add_name_to_users.rb
+++ db/migrate/20230327150905_add_name_to_users.rb
@@ -2,6 +2,6 @@
 
 class AddNameToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :name, :text
+    add_column :users, :name, :text, null: false
   end
 end
